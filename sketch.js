41d19063e6edb249f1ed79be9b4d7ecab3af206f
Rohanpande07 <78965170+Rohanpande07@users.noreply.github.com>
2021-05-27 11:05:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,41 +4,37 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
+
 var dustbinObj,groundObject, paperObject;
 var world;
 
 
 function setup() {
 	createCanvas(1600, 700);
-	rectMode(CENTER);
-
 
 	engine = Engine.create();
 	world = engine.world;
 	
 	groundObject = new ground(width/2,670,width,20);
 	dustbinObj = new dustbin(1200,650);
-	paperObject = new Paper(250,667,60,60);
-
-	Engine.run(engine);
+	paperObject = new Paper(250,667,40,40);
   
 }
 
 
 function draw() {
-  rectMode(CENTER);
   background(230);
- 
 
+  Engine.update(engine);
+ 
   groundObject.display();
   dustbinObj.display();
   paperObject.display();
-  keyPressed();
 }
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
-		Matter.Body.applyForce(paperObject.body, paperObject.body.position,{x:190,y:-50});
+		Matter.Body.applyForce(paperObject.body, paperObject.body.position,{x:150,y:-100});
    }
 }
 	
